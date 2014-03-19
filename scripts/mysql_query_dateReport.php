d54939cce2396c38166e88261dff0da7fde89b0c
Shavez <shavez00@yahoo.com>
2014-03-18 20:44:18 -0700

    Updated the sort order of the invoices to sort by invoice number instead of date

--- scripts/mysql_query_dateReport.php
+++ scripts/mysql_query_dateReport.php
@@ -1,5 +1,5 @@
 <?php
-	$invoiceDateList = mysql_query ("SELECT orders.date, orders.invoicenum, customers.name, orders.type, orders.size, orders.quantity, orders.price FROM orders INNER JOIN customers ON customers.custid = orders.custid WHERE orders.date >" . $start . " AND orders.date <" . $end . " ORDER BY orders.date");
+	$invoiceDateList = mysql_query ("SELECT orders.date, orders.invoicenum, customers.name, orders.type, orders.size, orders.quantity, orders.price FROM orders INNER JOIN customers ON customers.custid = orders.custid WHERE orders.date >" . $start . " AND orders.date <" . $end . " ORDER BY orders.invoicenum");
 	
 	while ($row = mysql_fetch_array($invoiceDateList)) {
 		$date[] = $row['date'];
